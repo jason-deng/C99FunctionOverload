0d67d175465b73dfb856f6a10cc2f9b2c11ce9dc
jason-deng <chunhui.deng@cxria.com>
2015-01-22 19:38:56 +0800

    main.c: Redefined macros to a more "overloading" fashion
    Macros with less arguments call the ones with more arguments
    recursively, so as to be "overloading".

--- main.c
+++ main.c
@@ -5,15 +5,13 @@ void realCreate(int x, int y)
   printf("(%d, %d)\n", x, y);
 }
 
-#define CREATE_0() realCreate(0, 0)
-#define CREATE_1(x) realCreate(1, x)
 #define CREATE_2(x, y) realCreate(x, y)
+#define CREATE_1(x) CREATE_2(x, 0)
+#define CREATE_0() CREATE_1(0)
 
 #define FUNC_CHOOSER(_f1, _f2, _f3, ...) _f3
 #define FUNC_RECOMPOSER(argsWithParentheses) FUNC_CHOOSER argsWithParentheses
-
 #define CHOOSE_FROM_ARG_COUNT(...) FUNC_RECOMPOSER((__VA_ARGS__, CREATE_2, CREATE_1, ))
-
 #define NO_ARG_EXPANDER() ,,CREATE_0
 #define MACRO_CHOOSER(...) CHOOSE_FROM_ARG_COUNT(NO_ARG_EXPANDER __VA_ARGS__ ())
 #define create(...) MACRO_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
